a8cd8b2232b563c5917a99b2a879a0045782ddae
RibeiroDev <ribeirogui.com@gmail.com>
2020-01-28 12:15:43 -0300

    novos exercícios

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 100
+
+console.log(`Você passou a ${vel} Km/h`)
+
+if (vel > 60) { //condição simples
+    console.log ('Vocé foi Multado!...Seninha')
+}
+
+console.log('Ande sempre com o cinto de segurança!')
---
+++ aula11/ex009.js
@@ -0,0 +1,10 @@
+var pais = 'frança'
+
+console.log(`Vivendo na ${pais}...`)
+
+if(pais != 'brasil') {
+    console.log('Você é estrangeiro!')
+}
+else {
+    console.log('Você é brasileiro!')
+}
